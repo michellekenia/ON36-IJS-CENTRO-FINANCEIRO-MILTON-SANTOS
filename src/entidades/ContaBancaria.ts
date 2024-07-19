753f51a753a62837b1f75117b5ed1521d04b9563
michellekenia <keniamsoaress@gmail.com>
2024-07-19 09:59:54 -0300

    criar-metodos

--- src/entidades/ContaBancaria.ts
+++ src/entidades/ContaBancaria.ts
@@ -1,4 +1,4 @@
-class ContaBancaria {
+abstract class ContaBancaria {
     
     protected numeroConta: string
     protected idAgencia: number    
@@ -16,5 +16,19 @@ class ContaBancaria {
         this.tipoConta = tipoConta
         
     }
+
+    //Método para consultar saldo 
+    public consultarSaldo(): number {
+        return this.saldo
+    }
+
+    //Método abstrato para sacar valor da conta
+    public abstract sacar(valor: number): void
+
+    //Método abstrato para depositar valor da conta
+    public abstract depositar(valor: number): void
+
+    //Método abstrato para transferir valor da conta para outra
+    public abstract transferir(valor: number, contaDestino: ContaBancaria): void
 
 }
