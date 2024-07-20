7ab77e007a54520b8ed484ae1ce1f1197df73335
michellekenia <keniamsoaress@gmail.com>
2024-07-19 23:09:47 -0300

    implementar-metodos

--- src/entidades/ContaCorrente.ts
+++ src/entidades/ContaCorrente.ts
@@ -9,14 +9,33 @@ class ContaCorrente extends ContaBancaria {
     }
 
     public sacar(valor: number): void {
-    
+        if (valor <= 0 ) {
+            throw new Error ("O valor deve ser maior do que zero.")
+        } if (valor > (this.saldo + this.limiteChequeEspecial)) {
+            throw new Error ("Você não tem saldo o suficiente.")
+        } else {
+            this.saldo -= valor
+        }
+        
     }
 
     public depositar(valor: number): void {
+        if (valor > 0) {
+            this.saldo += valor 
+        }  else {
+        throw new Error ("O valor deve ser maior do que zero.")
+        }
+    
     }
 
     public transferir(valor: number, contaDestino: ContaBancaria): void {
-        
+        if (valor < this.saldo + this.limiteChequeEspecial) {
+            this.sacar(valor)
+            contaDestino.depositar(valor)
+        }  else {
+        throw new Error ("Não há saldo suficiente.")
     }
-    
+
+    }
+
 }
