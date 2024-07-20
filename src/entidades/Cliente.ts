4ef7153b23fb48c6350e8bd88ac5af053dadb6bf
michellekenia <keniamsoaress@gmail.com>
2024-07-19 21:52:35 -0300

    incluir-conta

--- src/entidades/Cliente.ts
+++ src/entidades/Cliente.ts
@@ -3,12 +3,14 @@ class Cliente {
    private id: number
    private endereco: string
    private telefone: string
+   private conta: ContaBancaria
 
-    constructor(nome: string, id: number, endereco: string, telefone: string){
+    constructor(nome: string, id: number, endereco: string, telefone: string, conta: ContaBancaria){
         this.nome = nome
         this.id = id
         this.endereco = endereco
         this.telefone = telefone
+        this.conta = conta
 
     }
 
@@ -28,5 +30,9 @@ class Cliente {
         return this.telefone 
     }
 
+    public getConta(): ContaBancaria {
+        return this.conta
+    }
+    
 
 }
